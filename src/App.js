6bc42cd8aaf4d9aada87fda7476d466eeaa640b1
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-25 11:39:11 -0500

    Incorpotated sidebar into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-
-  </div>;
+  return (
+    <div>
+      <Router>
+        <NavBar />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
