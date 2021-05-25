602a8d86420e4d743be58d26e0c8890c8a392a8a
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-25 12:45:02 -0500

    Imported SideBar component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import NavBar from "./Components/NavBar";
+import NavBar from "./Components/SideBar";
 import { BrowserRouter as Router } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
       <Router>
         <NavBar />
+       
       </Router>
     </div>
   );
