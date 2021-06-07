336fdaf1c5a93edb6c48de21e9a78ee5bd7da11d
Janneth Verduzco <janneth_verduzco@live.com>
2021-06-07 14:20:30 -0500

    Removed unused files(reportWebVitals.js, setupTests.js). Replaced sideBar with regular top page navbar (have yet to style it)

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,67 +1,57 @@
 import React from "react";
-import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
-} from "cdbreact";
-import { Link } from "react-router-dom";
 import "./style.css";
-import Logo from "../../Assets/logo.png";
+import * as ReactBootStrap from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-function Sidebar() {
+function Navbar() {
   return (
-    <nav
-      style={{
-        display: "flex",
-        height: "100vh",
-        overflow: "auto",
-        // flexDirection: "column",
-        position: "fixed",
-      }}
-    >
-      <CDBSidebar textColor="white" backgroundColor="#64BB6A">
-        <CDBSidebarHeader>
-          {/* prefix={<i className="fa fa-bars fa-large"></i>}  */}
-
-          <a href="/about">
-            <img className="logo" src={Logo} alt="logo"></img>
-          </a>
-        </CDBSidebarHeader>
-
-        <CDBSidebarContent className="sidebar-content">
-          <CDBSidebarMenu>
-            <Link to="/about" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">About Us</CDBSidebarMenuItem>
+    <ReactBootStrap.Navbar className="navigationBar" bg="dark" expand="md"  variant="dark">
+      <ReactBootStrap.Navbar.Brand>
+        Janneth Alvarado Verduzco
+      </ReactBootStrap.Navbar.Brand>
+      <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+        <ReactBootStrap.Nav className="mr-auto">
+          <ReactBootStrap.Nav.Link>
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
             </Link>
-            <Link to="/services" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Services</CDBSidebarMenuItem>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link>
+            <Link
+              to="/portfolio"
+              className={
+                window.location.pathname === "/portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Portfolio
             </Link>
-            <Link to="/contact" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Contact Us</CDBSidebarMenuItem>
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link>
+            <Link
+              to="/contact"
+              className={
+                window.location.pathname === "/contact"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Contact
             </Link>
-            <Link to="/faqs" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="fa fa-question-circle">
-                FAQ's
-              </CDBSidebarMenuItem>
-            </Link>
-          </CDBSidebarMenu>
-        </CDBSidebarContent>
-
-        <CDBSidebarFooter style={{ textAlign: "center" }}>
-          <div
-            style={{
-              padding: "20px 5px",
-            }}
-          >
-            @ 2021 PROPAL LLC
-          </div>
-        </CDBSidebarFooter>
-      </CDBSidebar>
-    </nav>
+          </ReactBootStrap.Nav.Link>
+        </ReactBootStrap.Nav>
+      </ReactBootStrap.Navbar.Collapse>
+    </ReactBootStrap.Navbar>
   );
 }
 
-export default Sidebar;
+export default Navbar;
