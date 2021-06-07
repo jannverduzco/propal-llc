10ed8b2151469cec2be1fc6c5a5f9a68e3f94ccb
Janneth Verduzco <janneth_verduzco@live.com>
2021-06-07 16:05:51 -0500

    NPM installed react-icons. Imported mobile responsive toggle NavBar icons (FaBars, FaRegWIndowClose) and set an inital state, implemented a function and used a ternary operator to implemented

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdFingerprint } from "react-icons/md";
-// import { FaBars } from "react-icons/fa";
+import { FaBars, FaRegWindowClose } from "react-icons/fa";
 // import * as ReactBootStrap from "react-bootstrap";
 import "./style.css";
 
 function Navbar() {
+  // Setting initial valie of click to false
+  const [click, setClick] = useState(false);
+  // Creating a function to hanlde the click instance (Setting click to oppisite valie of click to toggle)
+  const handleClick = () => setClick(!click);
   return (
     <>
       <div className="navbar">
@@ -13,7 +17,9 @@ function Navbar() {
           <Link to="/" className="navbar-logo">
             <MdFingerprint className="navbar-icon" />
           </Link>
-          <div className="menu-icon"></div>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaRegWindowClose /> : <FaBars />}
+          </div>
         </div>
       </div>
     </>
