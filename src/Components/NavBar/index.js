71a0b10365761a0e10208516a1f365f42d254956
Janneth Verduzco <janneth_verduzco@live.com>
2021-06-08 15:23:08 -0500

    Added all navbar buttons with link (Home, About, Services etc..)

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -20,6 +20,23 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
             {click ? <FaRegWindowClose /> : <FaBars />}
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">HOME</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links">ABOUT</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-links">SERVICES</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-links">CONTACT</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/faqs" className="nav-links">FAQ'S</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </>
