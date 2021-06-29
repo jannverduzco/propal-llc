4a98239afef6d2efb9e7992fecd1963bc8fed216
Janneth Verduzco <janneth_verduzco@live.com>
2021-06-29 15:26:29 -0500

    Completed services page template

--- src/Components/Pages/Services/index.js
+++ src/Components/Pages/Services/index.js
@@ -1,13 +1,35 @@
 import React from "react";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Services() {
   return (
-    <div style={{ textAlign: "center" }}>
-  
-      <div>Our Services</div>
-  
-  </div>
+    <Container>
+      <style>
+        {
+          "body {background-image: url('https://images.pexels.com/photos/6863191/pexels-photo-6863191.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'); background-repeat: no-repeat; background-size: cover;  height: 100vh;}"
+        }
+      </style>
+
+      <Col className="page-container">
+        <Row className="page-title"> Our Services </Row>
+        <Row>
+          <Col className="page-content">
+            <ul>
+              <li>
+                <b>Income Tax</b>
+              </li>
+              *** Detailed Explanation Here ***
+              <li>
+                <b>Bookkeeping</b>
+              </li>
+              *** Detailed Explanation Here ***
+            </ul>
+          </Col>
+        </Row>
+      </Col>
+    </Container>
   );
 }
 
