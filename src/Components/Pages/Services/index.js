1fb8eb3a05d7001cf35b9ba47f44bdbe0ba02224
Janneth Verduzco <janneth_verduzco@live.com>
2021-07-07 15:45:25 -0500

    npm install bootstrap package. Inserted bootstrap css link in index.html. Used react bootstrap card template in service page.

--- src/Components/Pages/Services/index.js
+++ src/Components/Pages/Services/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
 import "./style.css";
 
 function Services() {
@@ -14,32 +15,49 @@ function Services() {
       </style>
 
       <Col className="services-container">
-        <Row className="page-title"> Our Services </Row>
-        <Row>
-          <Col className="page-content">
-            <ol type="1">
-              <b>
-                <li>Income Tax Preparation and Planning</li>
-              </b>
-              <ol type="a">
-                <li>
-                  We provide tax preparation services for <b>businesses</b> and{" "}
-                  <b>individuals</b>. A few common forms we prepare include:
-                </li>
-                <li>State returns, If needed.</li>
-                <li>
-                  Foreign reporting of income, assets, and other disclosures.
-                </li>
-                <li>IRS Audits</li>
-                <li>IRS or State Notices</li>
-              </ol>
-              <b>
-                <li>Bookkeeping Services</li>
-              </b>
-              <b></b><li>Business Planning</li>
-            </ol>
-          </Col>
-        </Row>
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{" "}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
       </Col>
     </Container>
   );
