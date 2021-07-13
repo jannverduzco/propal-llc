7d9e66f649e963f1c52058d97660034dd615ef5c
Janneth Verduzco <janneth_verduzco@live.com>
2021-07-12 22:44:45 -0500

    removed unused NavBar style.css code

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -21,7 +21,7 @@ function Navbar() {
       <div className="navbar">
         <div className="navbar-container container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            <h4>PROPAL LLC</h4>
+            <h3>PROPAL LLC</h3>
 
             
             {/* <img className="logo" src={Logo} alt="logo"></img> */}
