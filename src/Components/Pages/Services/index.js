873076b906650ccbf80df8654a710a48cbdb1a7c
Janneth Verduzco <janneth_verduzco@live.com>
2021-07-13 15:08:58 -0500

    Imported image for pages/services card 'num 1'. Removed 'position:stciky' from navbar. Card 'num 1' text completed.

--- src/Components/Pages/Services/index.js
+++ src/Components/Pages/Services/index.js
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import "./style.css";
+import TaxPrep from "../../../Assets/taxPrep.png";
 
 function Services() {
   return (
@@ -17,18 +18,38 @@ function Services() {
       <Col className="services-container">
         <CardDeck>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img className="tax-prep" variant="top" src={TaxPrep} />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>
+                <b>Income Tax Preparation and Planning</b>
+              </Card.Title>
               <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                <ul>
+                  <li>
+                    We provide tax preparation services for business and
+                    individuals. A few common forms we prepare include:
+                  </li>
+                  <ul>
+                    <li>1040 - Individual Income Tax Return</li>
+                    <li>1040NR - Non-Resident Individual Income Tax Return</li>
+                    <li>1120 - Corporate Income Tax Return</li>
+                    <li>1120S - Subchapter S Corporate Income Tax Return</li>
+                    <li>1065 - Partnership Income Tax ReturnFAQ’s</li>
+                    <li>
+                      7002 - Application for Individual Taxpayer Identification
+                      Number (ITIN)
+                    </li>
+                  </ul>
+                  <li>State returns, if needed</li>
+                  <li>
+                    Foreign reporting of income, assets, and other disclosures
+                  </li>
+                  <li>IRS Audits</li>
+                  <li>IRS or State Notices</li>
+                </ul>
               </Card.Text>
+              <Card.Text></Card.Text>
             </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
           </Card>
           <Card>
             <Card.Img variant="top" src="holder.js/100px160" />
@@ -39,9 +60,6 @@ function Services() {
                 additional content.{" "}
               </Card.Text>
             </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
           </Card>
           <Card>
             <Card.Img variant="top" src="holder.js/100px160" />
@@ -53,9 +71,6 @@ function Services() {
                 than the first to show that equal height action.
               </Card.Text>
             </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
           </Card>
         </CardDeck>
       </Col>
