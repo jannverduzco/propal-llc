0c12560c5e5744ef07f9e654a3d170f84f21e466
Janneth Verduzco <janneth_verduzco@live.com>
2021-07-20 15:27:50 -0500

    setting contact template

--- src/Components/Pages/Contact/index.js
+++ src/Components/Pages/Contact/index.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
 
 function Contact() {
   return (
-    <div className="container border"
-    s>
-      <div>Contact Us</div>
-      <form>
-        <label>First Name</label>
-        <input type="first-name" name="first-name" />
-        <label>Last Name</label>
-        <input type="last-name" name="last-name" />
-        <label>Phone Number</label>
-        <input type="phone-number" name="phone-number" />
+    <div className="container">
+      <form className="form">
+        <h1>Contact Us</h1>
+
+        <label>Name</label>
+        <input
+          placeholder="Name"
+          // value={name}
+          // onChange={(e) => setName(e.target.value)}
+        />
+
         <label>Email</label>
-        <input type="email" name="email" />
+        <input
+          placeholder="Email"
+          // value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+        />
+
         <label>Message</label>
-        <textarea name="message" rows="4"></textarea>
-        <input type="submit" value="send" />
+        <textarea
+          placeholder="Message"
+          // value={message}
+          // onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+
+        <button
+          type="submit"
+          // style={{ background: loader ? "#ccc" : " rgb(2, 2, 110)" }}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
