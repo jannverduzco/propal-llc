26c7f097fdd3f1d06bd07123fa350e3a56815bf7
Janneth Verduzco <janneth_verduzco@live.com>
2021-08-03 14:41:42 -0500

    removed Bootstap CSS link from index.html. Removed 'FAQS' page from App.js.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Pages/Home/index.js";
 import About from "./Components/Pages/About/index.js";
 import Services from "./Components/Pages/Services/index.js";
-import FAQS from "./Components/Pages/Faqs/index.js";
 import Contact from "./Components/Pages/Contact/index.js";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route exact path="/about" component={About} />
         <Route exact path="/services" component={Services} />
         <Route exact path="/contact" component={Contact} />
-        <Route exact path="/faqs" component={FAQS} />
       </Switch>
     </Router>
   );
--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -7,57 +7,63 @@ import { IconContext } from "react-icons/lib";
 // import * as ReactBootStrap from "react-bootstrap";
 
 function Navbar() {
-  // Setting initial valie of click to false
+  // Setting initial value of click to false
   const [click, setClick] = useState(false);
 
   // Creating a function to hanlde the click instance (Setting click to oppisite valie of click to toggle)
   const handleClick = () => setClick(!click);
   // Creating a function to hable the mobile click instance propting navbar to dissapear and 'clicked page' to reflect
-  const closeMobileMenu = () => setClick(false)
+  const closeMobileMenu = () => setClick(false);
   return (
     <>
-    {/* Wrapping everything in one color using IconContext */}
-    <IconContext.Provider value={{ color: '#fff'}}>
-      <div className="navbar">
-        <div className="navbar-container container">
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            <h3>PROPAL LLC</h3>
+      {/* Wrapping everything in one color using IconContext */}
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+              <h3>PROPAL LLC</h3>
 
-            
-            {/* <img className="logo" src={Logo} alt="logo"></img> */}
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <FaRegWindowClose /> : <FaBars />}
+              {/* <img className="logo" src={Logo} alt="logo"></img> */}
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaRegWindowClose /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  HOME
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/about"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  ABOUT
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/services"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  SERVICES
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/contact"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                HOME
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-                ABOUT
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
-                SERVICES
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
-                CONTACT
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/faqs" className="nav-links" onClick={closeMobileMenu}>
-                FAQ'S
-              </Link>
-            </li>
-          </ul>
         </div>
-      </div>
       </IconContext.Provider>
     </>
   );
