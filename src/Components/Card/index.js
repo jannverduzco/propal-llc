828885a0f4ae80b757725e06d7132390c7930393
Janneth Verduzco <janneth_verduzco@live.com>
2021-08-21 15:07:55 -0500

    Added contents to card component. updated card style as needed. Deleted unused css styling

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./style.css";
 
-function Card({title, imageUrl, body}) {
+function Card({
+  title,
+  imageUrl,
+  body,
+  list1,
+  list2,
+  list3,
+  list4,
+  list5,
+  sublist1,
+  sublist2,
+  sublist3,
+  sublist4,
+  sublist5,
+  sublist6,
+}) {
   return (
     <div className="card-container">
       <div className="image-container">
@@ -9,9 +24,32 @@ function Card({title, imageUrl, body}) {
       </div>
       <div className="card-content">
         <div className="card-title">
-          <h4>{title}</h4>
+          <h3>{title}</h3>
         </div>
-        <div className="card-body">{body}</div>
+        <div className="card-body">
+          <h5>{body}</h5>
+          <div className="card-list">
+            <ul>
+              <li>
+                {list1}
+                <ul>
+                  <h5>
+                    <li>{sublist1}</li>
+                    <li>{sublist2}</li>
+                    <li>{sublist3}</li>
+                    <li>{sublist4}</li>
+                    <li>{sublist5}</li>
+                    <li>{sublist6}</li>
+                  </h5>
+                </ul>
+              </li>
+              <li>{list2}</li>
+              <li>{list3}</li>
+              <li>{list4}</li>
+              <li>{list5}</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Pages/Services/index.js
+++ src/Components/Pages/Services/index.js
@@ -7,120 +7,140 @@ import BusinessPlanning from "../../../Assets/business-planning.png";
 
 function Services() {
   return (
-    <Card
-      title="Income Tax Preparation and Planning"
-      imageUrl="https://mycountsolutions.com/wp-content/uploads/2020/04/tax-preparation-services.png"
-      body="We provide tax preparation services for business and  individuals. A few common forms we prepare include:
-1040 - Individual Income Tax ReturnS
-1040NR - Non-Resident Individual Income Tax Return
-               1120 - Corporate Income Tax Return
-               1120S - Subchapter S Corporate Income Tax Return
-               1065 - Partnership Income Tax Return
-               
-                 7002 - Application for Individual Taxpayer Identification
-                 Number (ITIN)
-               
-            
-             State returns, if needed
-             
-               Foreign reporting of income, assets, and other disclosures
-             
-             IRS Audits
-             IRS or State Notices
-"
-    />
-
-    // <Container>
-    //   <style>
-    //     {
-    //       "body {background-color: #c9e1d5; background-repeat: no-repeat; ;  height: 100vh;}"
-    //     }
-    //   </style>
-
-    //   <Col className="services-container">
-    //     <CardDeck>
-    //       <Card>
-    //         <Card.Img className="tax-prep" variant="top" src={TaxPrep} />
-    //         <Card.Body>
-    //           <Card.Title>
-    //             <b>Income Tax Preparation and Planning</b>
-    //           </Card.Title>
-    //           <Card.Text>
-    //             <ul>
-    //               <li>
-    //                 We provide tax preparation services for business and
-    //                 individuals. A few common forms we prepare include:
-    //               </li>
-    //               <ul>
-    //                 <li>1040 - Individual Income Tax Return</li>
-    //                 <li>1040NR - Non-Resident Individual Income Tax Return</li>
-    //                 <li>1120 - Corporate Income Tax Return</li>
-    //                 <li>1120S - Subchapter S Corporate Income Tax Return</li>
-    //                 <li>1065 - Partnership Income Tax Return</li>
-    //                 <li>
-    //                   7002 - Application for Individual Taxpayer Identification
-    //                   Number (ITIN)
-    //                 </li>
-    //               </ul>
-    //               <li>State returns, if needed</li>
-    //               <li>
-    //                 Foreign reporting of income, assets, and other disclosures
-    //               </li>
-    //               <li>IRS Audits</li>
-    //               <li>IRS or State Notices</li>
-    //             </ul>
-    //           </Card.Text>
-    //           <Card.Text></Card.Text>
-    //         </Card.Body>
-    //       </Card>
-    //       <Card>
-    //         <Card.Img className="bookkeeping" variant="top" src={Bookkeeping} />
-    //         <Card.Body>
-    //           <Card.Title>
-    //             <b>Bookkeeping Services</b>
-    //           </Card.Title>
-    //           <Card.Text>
-    //             We provide the following bookkeeping services:
-    //             <ul>
-    //               <li>Preparation of Business Reports</li>
-    //               <li>Quickbooks Training</li>
-    //               <li>Payroll Tax preparation</li>
-    //               <li>Sales Tax preparations</li>
-    //             </ul>
-    //           </Card.Text>
-    //         </Card.Body>
-    //       </Card>
-    //       <Card>
-    //         <Card.Img
-    //           className="business-planning"
-    //           variant="top"
-    //           src={BusinessPlanning}
-    //         />
-    //         <Card.Body>
-    //           <Card.Title>
-    //             <b>Business Planning</b>
-    //           </Card.Title>
-    //           <Card.Text>
-    //             We want all of our clients to be fully aware of the many options
-    //             one can take when it comes to financial planning. We can help
-    //             with questions such as:
-    //             <ul>
-    //             <li> Should I start an LLC? </li>
-    //             <li>What type of entity is best for me?</li>
-    //             <li> What tax breaks can I take advantage of?</li>
-    //             <li>
-    //               {" "}
-    //               How do I keep track of my records? How do I save money on
-    //               taxes?
-    //             </li>
-    //             </ul>
-    //           </Card.Text>
-    //         </Card.Body>
-    //       </Card>
-    //     </CardDeck>
-    //   </Col>
-    // </Container>
+    <div className="container">
+      <h1>Our Services</h1>
+      <Card
+        title="Income Tax Preparation and Planning"
+        imageUrl="https://mycountsolutions.com/wp-content/uploads/2020/04/tax-preparation-services.png"
+        list1="We provide tax preparation services for business and individuals. A
+      few common forms we prepare include:"
+        sublist1="1040 - Individual Income Tax Return"
+        sublist2="1040NR - Non-Resident Individual Income Tax Return"
+        sublist3="1120 - Corporate Income Tax Return"
+        sublist4="1120S - Subchapter S Corporate Income Tax Return"
+        sublist5="1065 - Partnership Income Tax Return"
+        sublist6="7002 - Application for Individual Taxpayer Identification Number
+      (ITIN)"
+        list2="State returns, if needed"
+        list3="Foreign reporting of income, assets, and other disclosures"
+        list4="IRS Audits"
+        list5="IRS or State Notices"
+      >
+        <ul>
+          <li>
+            We provide tax preparation services for business and individuals. A
+            few common forms we prepare include:
+          </li>
+          <ul>
+            <li>1040 - Individual Income Tax Return</li>
+            <li>1040NR - Non-Resident Individual Income Tax Return</li>
+            <li>1120 - Corporate Income Tax Return</li>
+            <li>1120S - Subchapter S Corporate Income Tax Return</li>
+            <li>1065 - Partnership Income Tax Return</li>
+            <li>
+              7002 - Application for Individual Taxpayer Identification Number
+              (ITIN)
+            </li>
+          </ul>
+          <li>State returns, if needed</li>
+          <li>Foreign reporting of income, assets, and other disclosures</li>
+          <li>IRS Audits</li>
+          <li>IRS or State Notices</li>
+        </ul>
+      </Card>
+    </div>
   );
 }
+
+// <Container>
+//   <style>
+//     {
+//       "body {background-color: #c9e1d5; background-repeat: no-repeat; ;  height: 100vh;}"
+//     }
+//   </style>
+
+//   <Col className="services-container">
+//     <CardDeck>
+//       <Card>
+//         <Card.Img className="tax-prep" variant="top" src={TaxPrep} />
+//         <Card.Body>
+//           <Card.Title>
+//             <b>Income Tax Preparation and Planning</b>
+//           </Card.Title>
+//           <Card.Text>
+//             <ul>
+//               <li>
+//                 We provide tax preparation services for business and
+//                 individuals. A few common forms we prepare include:
+//               </li>
+//               <ul>
+//                 <li>1040 - Individual Income Tax Return</li>
+//                 <li>1040NR - Non-Resident Individual Income Tax Return</li>
+//                 <li>1120 - Corporate Income Tax Return</li>
+//                 <li>1120S - Subchapter S Corporate Income Tax Return</li>
+//                 <li>1065 - Partnership Income Tax Return</li>
+//                 <li>
+//                   7002 - Application for Individual Taxpayer Identification
+//                   Number (ITIN)
+//                 </li>
+//               </ul>
+//               <li>State returns, if needed</li>
+//               <li>
+//                 Foreign reporting of income, assets, and other disclosures
+//               </li>
+//               <li>IRS Audits</li>
+//               <li>IRS or State Notices</li>
+//             </ul>
+//           </Card.Text>
+//           <Card.Text></Card.Text>
+//         </Card.Body>
+//       </Card>
+//       <Card>
+//         <Card.Img className="bookkeeping" variant="top" src={Bookkeeping} />
+//         <Card.Body>
+//           <Card.Title>
+//             <b>Bookkeeping Services</b>
+//           </Card.Title>
+//           <Card.Text>
+//             We provide the following bookkeeping services:
+//             <ul>
+//               <li>Preparation of Business Reports</li>
+//               <li>Quickbooks Training</li>
+//               <li>Payroll Tax preparation</li>
+//               <li>Sales Tax preparations</li>
+//             </ul>
+//           </Card.Text>
+//         </Card.Body>
+//       </Card>
+//       <Card>
+//         <Card.Img
+//           className="business-planning"
+//           variant="top"
+//           src={BusinessPlanning}
+//         />
+//         <Card.Body>
+//           <Card.Title>
+//             <b>Business Planning</b>
+//           </Card.Title>
+//           <Card.Text>
+//             We want all of our clients to be fully aware of the many options
+//             one can take when it comes to financial planning. We can help
+//             with questions such as:
+//             <ul>
+//             <li> Should I start an LLC? </li>
+//             <li>What type of entity is best for me?</li>
+//             <li> What tax breaks can I take advantage of?</li>
+//             <li>
+//               {" "}
+//               How do I keep track of my records? How do I save money on
+//               taxes?
+//             </li>
+//             </ul>
+//           </Card.Text>
+//         </Card.Body>
+//       </Card>
+//     </CardDeck>
+//   </Col>
+// </Container>
 
 export default Services;
